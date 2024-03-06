2ebaa87405ac83991324c989d69157fec93a0d9c
KamilFicerman <154465288+KamilFicerman@users.noreply.github.com>
2024-03-06 21:01:01 +0100

    czat - lepszy wygląd v2

---
+++ Aplikacja_MAUI/WMiIApp/WMiIApp/Converters/FromIsSentToMessageColorConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMiIApp
+{
+    class FromIsSentToMessageColorConverter : IValueConverter
+    {
+        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is bool isSent && isSent)
+            {
+                return Colors.DarkBlue;
+            }
+            else
+            {
+                return Colors.DimGray;
+            }
+        }
+
+        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
