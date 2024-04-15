7ff5bfc9ebb9a0e5889d8e46d337d05dcc9ff99a
Kacper Duma <129724409+RecoilCoding@users.noreply.github.com>
2024-04-15 12:14:13 +0200

    Dodano liste wszystkich pomieszczen

---
+++ Aplikacja_MAUI/WMiIApp/WMiIApp/Models/Room.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMiIApp.Models
+{
+    public class Room
+    {
+        public string Id { get; set; } 
+        public string Name { get; set; }
+        public double PositionX { get; set; }
+        public double PositionY { get; set; }
+        public int Floor { get; set; }
+        public List<Room> Neighbors { get; } = new List<Room>();
+        public List<string> Residents { get; } = new List<string>();
+        public string ScheduleImagePath { get; set; }
+
+        public Room(string id, string name, double positionX, double positionY, int floor)
+        {
+            Id = id;
+            Name = name;
+            PositionX = positionX;
+            PositionY = positionY;
+            Floor = floor;
+        }
+
+        public void AddNeighbor(Room neighbor)
+        {
+            if (!Neighbors.Contains(neighbor))
+            {
+                Neighbors.Add(neighbor);    
+            }
+        }
+
+        public void AddResident(string resident)
+        {
+            Residents.Add(resident);
+        }
+
+        // Metoda do wyswietlenia sasiadow w alercie
+        public void PrintNeighbors()
+        {
+            if (Neighbors.Count == 0)
+            {
+                // Brak sasiadow, wyswietl alert informujacy o tym
+                Application.Current.MainPage.DisplayAlert("Sąsiedzi", "Brak sąsiadów dla tego pomieszczenia.", "OK");
+            }
+            else
+            {
+                // Sasiedzi istnieja, wygeneruj wiadomosc z nazwami sasiadow
+                StringBuilder message = new StringBuilder();
+                message.AppendLine("Sąsiedzi dla pomieszczenia " + Name + ":");
+
+                foreach (var neighbor in Neighbors)
+                {
+                    message.AppendLine("- " + neighbor.Name);
+                }
+
+                // Wyswietl alert z lista sasiadow
+                Application.Current.MainPage.DisplayAlert("Sąsiedzi", message.ToString(), "OK");
+            }
+        }
+    }
+}
