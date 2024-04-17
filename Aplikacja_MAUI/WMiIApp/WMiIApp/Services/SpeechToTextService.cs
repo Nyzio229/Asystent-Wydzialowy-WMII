2af03efabcc760fcc16ac7afd9ca964dc359f0ea
KamilFicerman <154465288+KamilFicerman@users.noreply.github.com>
2024-04-17 22:18:56 +0200

    Rozbudowanie czatu
    dodanie SpeechToText, obsługi TextToSpeech po naciśnięciu wiadomości, obsługi SpeechToText po naciśnięciu mikrofonu
    pełny scenariusz zapytań na endpointy, drobne poprawki stylistyczne

---
+++ Aplikacja_MAUI/WMiIApp/WMiIApp/Services/SpeechToTextService.cs
@@ -0,0 +1,63 @@
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Media;
+using System.Globalization;
+using WMiIApp.ViewModels;
+
+
+//polski nie jest wspierany na windows
+
+//wywołanie:
+/*
+        [RelayCommand]
+        async Task Speak()
+        {
+            CancellationToken token = source.Token;
+            try
+            {
+                await SpeechToTextService.StartListening(token, this);
+            }
+            catch (Exception ex) 
+            {
+                await Shell.Current.DisplayAlert("Error!", ex.Message, "OK");
+            }
+        }
+ */
+
+//można dodać obsługę zatrzymania (na android niepotrzebna)
+
+namespace WMiIApp.Services
+{
+    public static class SpeechToTextService
+    {
+        public static async Task StartListening(CancellationToken cancellationToken, MainViewModel mvm)
+        {
+            var isGranted = await SpeechToText.RequestPermissions(cancellationToken);
+            if (!isGranted)
+            {
+                await Toast.Make("Brak uprawnień!").Show(CancellationToken.None);
+                return;
+            }
+
+            SpeechToText.Default.RecognitionResultUpdated += (sender, args) => OnRecognitionTextUpdated(sender, args, mvm);
+            SpeechToText.Default.RecognitionResultCompleted += (sender, args) => OnRecognitionTextCompleted(sender, args, mvm);
+            await SpeechToText.StartListenAsync(CultureInfo.GetCultureInfo("pl"), CancellationToken.None);
+        }
+
+        public static async Task StopListening(CancellationToken cancellationToken, MainViewModel mvm)
+        {
+            await SpeechToText.StopListenAsync(CancellationToken.None);
+            SpeechToText.Default.RecognitionResultUpdated -= (sender, args) => OnRecognitionTextUpdated(sender, args, mvm);
+            SpeechToText.Default.RecognitionResultCompleted -= (sender, args) => OnRecognitionTextCompleted(sender, args, mvm);
+        }
+
+        static void OnRecognitionTextUpdated(object? sender, SpeechToTextRecognitionResultUpdatedEventArgs args, MainViewModel mvm)
+        {
+            mvm.Text = args.RecognitionResult;
+        }
+
+        static void OnRecognitionTextCompleted(object? sender, SpeechToTextRecognitionResultCompletedEventArgs args, MainViewModel mvm)
+        {
+            mvm.Text = args.RecognitionResult;
+        }
+    }
+}
