e0ef9c5bf27a23edebe079ead88b6ae9bd2a1954
Nyzio <norja229@gmail.com>
2024-04-23 22:36:28 +0200

    Dodano dalszą część weryfikacji i utworzenie api key

---
+++ Serwer/ServerProjektu/ServerApiMikoAI/Models/ApiAccessTableContext.cs
@@ -0,0 +1,6 @@
+﻿namespace ServerApiMikoAI.Models {
+    public class ApiAccessTableContext {
+        public string device_id { get; set; }
+        public string api_key { get; set; }
+    }
+}
--- Serwer/ServerProjektu/ServerApiMikoAI/Models/VerificationDataBaseContext.cs
+++ Serwer/ServerProjektu/ServerApiMikoAI/Models/VerificationDataBaseContext.cs
@@ -5,8 +5,10 @@ namespace ServerApiMikoAI.Models {
         public VerificationDataBaseContext(DbContextOptions<VerificationDataBaseContext> options) : base(options) { }
 
         public DbSet<VerificationTableContext> verification_table { get; set; }
+        public DbSet<ApiAccessTableContext> api_access{ get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
             modelBuilder.Entity<VerificationTableContext>().HasKey(t => t.device_id);
+            modelBuilder.Entity<ApiAccessTableContext>().HasKey(t => t.device_id);
         }
     }
 }
