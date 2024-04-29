c048b6a64ab7ee1bf1a677bd8ceaaf55d26b4a6c
Kacper Duma <129724409+RecoilCoding@users.noreply.github.com>
2024-04-29 20:10:58 +0200

    Dodano menu wyboru pomieszczen do trasy

--- Aplikacja_MAUI/WMiIApp/WMiIApp/Views/MapPage2.xaml.cs
+++ Aplikacja_MAUI/WMiIApp/WMiIApp/Views/MapPage2.xaml.cs
@@ -1,13 +1,20 @@
+using CommunityToolkit.Maui.Core.Platform;
 using WMiIApp.Models;
 
 namespace WMiIApp;
 
 public partial class MapPage2 : ContentPage
 {
-	public MapPage2()
+    private List<string> allRoomNames; // Lista wszystkich nazw pokoi
+    private List<string> filteredRoomNames; // Lista nazw pokoi startowych po zastosowaniu filtru
+    public MapPage2()
 	{
 		InitializeComponent();
-	}
+
+        // Inicjalizacja listy nazw pokojow
+        allRoomNames = App.GlobalRooms.GetRooms().Select(room => room.Name).OrderBy(name => name).ToList();
+        filteredRoomNames = allRoomNames;
+    }
 
     private void HandleRoomButtonClick(object sender, EventArgs e)
     {
@@ -15,7 +22,7 @@ public partial class MapPage2 : ContentPage
         Button clickedButton = (Button)sender;
         string roomId = clickedButton.ClassId;
 
-        // ZnajdŸ sale w RoomMap
+        // Znajdz sale w RoomMap
         Room room = App.GlobalRooms.GetRoomById(roomId);
 
         if (room != null)
@@ -25,38 +32,32 @@ public partial class MapPage2 : ContentPage
 
         }
     }
-    private void HandleRoomButtonClickOnScreen(object sender, EventArgs e)
-    {
-        // Pobierz identyfikator kliknietego przycisku
-        Button clickedButton = (Button)sender;
-        string roomId = clickedButton.ClassId;
 
-        // ZnajdŸ sale w RoomMap
-        Room room = App.GlobalRooms.GetRoomById(roomId);
-
-        if (room != null)
-        {
-            // Wyswietl informacje o sali
-            DisplayRoomInfoOnScreen(room);
-        }
-    }
-
-    private void DisplayRoomInfo(Room room)
+    private async void DisplayRoomInfo(Room room)
     {
         string message = $"Nazwa sali: {room.Name}\n" +
                          $"Piêtro: {room.Floor}\n";
 
+        // Wyswietlanie innych nazw, jesli s¹ dostêpne
+        if (room.OtherNames.Any())
+        {
+            message += "Inne Nazwy: ";
+            foreach (string otherName in room.OtherNames)
+            {
+                message += $" {otherName} ";
+            }
+            message += "\n";
+        }
+
         if (room.Residents.Any())
         {
-            message += "Rezydenci:\n";
+            message += "Rezydenci: ";
             foreach (string resident in room.Residents)
             {
-                message += $"- {resident}\n";
+                message += $" {resident} ";
             }
-        }
-        else
-        {
-            message += "Nikt nie posiada tutaj gabinetu.\n";
+
+            message += "\n";
         }
 
         // Jesli plan zajec jest dostepny, dodaj go do wiadomosci
@@ -64,48 +65,127 @@ public partial class MapPage2 : ContentPage
         {
             message += "Plan zajêæ:\n" + room.ScheduleImagePath + "\n";
         }
-        else
-        {
-            message += "Brak planu zajêæ dla tej sali.\n";
-        }
 
-        // Ustawiamy tekst 
-        DisplayAlert("Pomieszczenie", message, "OK");
-    }
-    private void DisplayRoomInfoOnScreen(Room room)
-    {
-        string message = $"Nazwa sali: {room.Name}\n" +
-                         $"Piêtro: {room.Floor}\n";
-
-        if (room.Residents.Any())
+        // Wyswietlanie alertu z dodatkowa opcj¹
+        bool isTrasaClicked = await DisplayAlert("Pomieszczenie", message, "OK", "Trasa");
+        if (!isTrasaClicked)
         {
-            message += "Rezydenci:\n";
-            foreach (string resident in room.Residents)
+            // Wyswietlanie arkusza dzialan z dwiema opcjami
+            string actionResult = await DisplayActionSheet("Wybierz akcjê", "Anuluj", null, "Dodaj jako miejsce startowe", "Dodaj jako miejsce docelowe");
+            if (actionResult == "Dodaj jako miejsce startowe")
             {
-                message += $"- {resident}\n";
+                sourceRoomListView.SelectedItem = room.Name;
+            }
+            else if (actionResult == "Dodaj jako miejsce docelowe")
+            {
+                destinationRoomListView.SelectedItem = room.Name;
             }
         }
-        else
-        {
-            message += "Nikt nie posiada tutaj gabinetu.\n";
-        }
-
-        // Jesli plan zajec jest dostepny, dodaj go do wiadomosci
-        if (!string.IsNullOrEmpty(room.ScheduleImagePath))
-        {
-            message += "Plan zajêæ:\n" + room.ScheduleImagePath + "\n";
-        }
-        else
-        {
-            message += "Brak planu zajêæ dla tej sali.\n";
-        }
-
-        // Ustawiamy tekst w kontrolce Label
-        RoomInfoLabel.Text = message;
-
-        double screenWidth = Width / 1.34;
-        double screenHeight = Height / 1.34;
-        Content.WidthRequest = screenWidth;
-        Content.HeightRequest = screenHeight;
     }
+
+    private void OnSourceSearchBarTextChanged(object sender, TextChangedEventArgs e)
+    {
+        if (!string.IsNullOrEmpty(sourceSearchBar.Text))
+        {
+            string filterText = sourceSearchBar.Text.ToLower();
+            List<string> filteredRoomNames = allRoomNames.Where(roomName => roomName.ToLower().Contains(filterText)).ToList();
+            sourceRoomListView.ItemsSource = filteredRoomNames;
+            sourceRoomListView.IsVisible = true;
+        }
+        else
+        {
+            sourceRoomListView.IsVisible = false; // Ukrywamy CollectionView gdy tekst jest pusty
+        }
+    }
+
+    private void OnSourceRoomListViewSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (e.CurrentSelection.FirstOrDefault() != null)
+        {
+            string selectedRoom = (string)e.CurrentSelection.FirstOrDefault();
+
+            sourceRoomListView.IsVisible = false;
+
+            sourceSearchBar.Text = selectedRoom;
+
+            sourceSearchBar.HideKeyboardAsync();
+        }
+    }
+
+    private void OnDestinationSearchBarTextChanged(object sender, TextChangedEventArgs e)
+    {
+        if (!string.IsNullOrEmpty(destinationSearchBar.Text))
+        {
+            string filterText = destinationSearchBar.Text.ToLower();
+            List<string> filteredRoomNames = allRoomNames.Where(roomName => roomName.ToLower().Contains(filterText)).ToList();
+            destinationRoomListView.ItemsSource = filteredRoomNames;
+            destinationRoomListView.IsVisible = true;
+        }
+        else
+        {
+            destinationRoomListView.IsVisible = false; // Ukrywamy CollectionView gdy tekst jest pusty
+        }
+    }
+
+
+    private void OnDestinationRoomListViewSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (e.CurrentSelection.FirstOrDefault() != null)
+        {
+            string selectedRoom = (string)e.CurrentSelection.FirstOrDefault();
+
+            destinationRoomListView.IsVisible = false;
+
+            destinationSearchBar.Text = selectedRoom;
+
+            destinationSearchBar.HideKeyboardAsync();
+        }
+    }
+
+    // Metoda wywo³ywana po kliknieciu przycisku "Wyznacz trase"
+    private void OnCalculateRouteClicked(object sender, EventArgs e)
+    {
+        string sourceRoomName = (string)sourceRoomListView.SelectedItem;
+        string destinationRoomName = (string)destinationRoomListView.SelectedItem;
+
+        // Pobieramy obiekty pokoi na podstawie ich nazw
+        Room sourceRoom = App.GlobalRooms.FindByName(sourceRoomName);
+        Room destinationRoom = App.GlobalRooms.FindByName(destinationRoomName);
+
+        if (sourceRoom == null || destinationRoom == null)
+        {
+            // Niepoprawne argumenty, wyœwietlamy alert
+            DisplayAlert("B³¹d", "Nie mo¿na znaleŸæ pokoju Ÿród³owego lub docelowego.", "OK");
+            return;
+        }
+
+        // Znajdujemy najkrotsz¹ sciezke miêdzy pokojami
+        List<Room> shortestPath = App.pathFinder.FindShortestPath(sourceRoom, destinationRoom);
+
+        if (shortestPath == null)
+        {
+            // Nie udalo siê znalezc sciezki, wyswietlamy alert
+            DisplayAlert("B³¹d", "Nie mo¿na znaleŸæ œcie¿ki.", "OK");
+        }
+        else
+        {
+            // Znaleziono sciezke, wyswietlamy alert z lista pokoi
+            string message = "Znaleziona droga:\n";
+            foreach (var room in shortestPath)
+            {
+                message += room.Name + "\n";
+            }
+            DisplayAlert("Znaleziona droga", message, "OK");
+        }
+    }
+
+    private void OnShowMenuButtonClicked(object sender, EventArgs e)
+    {
+        // Zmien widocznosc okna menu
+        if (menuGrid.IsVisible == false)
+            menuGrid.IsVisible = true;
+        else menuGrid.IsVisible = false;
+    }
+
+
 }
