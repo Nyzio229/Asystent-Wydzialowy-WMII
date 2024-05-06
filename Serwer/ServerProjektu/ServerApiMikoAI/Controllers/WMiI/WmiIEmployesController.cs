1da6e43208a987a0e08175ef6be531ea1bf79c6c
Nyzio <norja229@gmail.com>
2024-05-06 21:48:54 +0200

    Dodano nowy endpoint z tablicą pracowników
    endpoint: /WmiIEmployes

---
+++ Serwer/ServerProjektu/ServerApiMikoAI/Controllers/WMiI/WmiIEmployesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Org.BouncyCastle.Crypto.Modes.Gcm;
+using ServerApiMikoAI.Models.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+using System.Data;
+
+namespace ServerApiMikoAI.Controllers.WMiI {
+    [Route("[controller]")]
+    [ApiController]
+    public class WmiIEmployesController : ControllerBase {
+        private readonly WMiIDataBase _context;
+        public WmiIEmployesController(WMiIDataBase context) {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetEmployeesStatus() {
+
+            try {
+                var lampki = _context.lampki.Select(lampka => new {
+                    Name = (lampka.pole1 ?? "") + " " + (lampka.pole2 ?? "") + " " + (lampka.pole3 ?? ""),
+                    Office = lampka.pole4 ?? "",
+                    IsWorking = lampka.stan
+                }).ToList();
+
+                return Ok(lampki);
+            }
+            catch (Exception ex) {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Serwer/ServerProjektu/ServerApiMikoAI/Models/Context/WMiIDatabase.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ServerApiMikoAI.Models.Context {
+    public class WMiIDataBase : DbContext {
+        public WMiIDataBase(DbContextOptions<WMiIDataBase> options) : base(options) {}
+
+        public DbSet<WMiIEmployessTable> lampki { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<WMiIEmployessTable>().HasKey(t => t.numerLampki);
+        }
+    }
+}
