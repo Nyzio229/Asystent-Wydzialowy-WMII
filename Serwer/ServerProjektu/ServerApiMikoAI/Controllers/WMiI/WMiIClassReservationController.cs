6a298884b272f0fdcdda0a5844c583897a04d74a
Nyzio <norja229@gmail.com>
2024-05-07 19:24:35 +0200

    Dodano wypisywanie rezerwacji sal na podany dzien

---
+++ Serwer/ServerProjektu/ServerApiMikoAI/Controllers/WMiI/WMiIClassReservationController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServerApiMikoAI.Models.Context;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace ServerApiMikoAI.Controllers.WMiI {
+    [Route("[controller]")]
+    [ApiController]
+    public class WMiIClassReservationController : ControllerBase {
+        private readonly WMiIPlansDatabase _context;
+        public WMiIClassReservationController(WMiIPlansDatabase context) {
+            _context = context;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        [SwaggerOperation(OperationId = "post")]
+        public async Task<IActionResult> GetClassReservation(string date) {
+            try {
+                if (!DateTime.TryParse(date, out DateTime parsedDate)) {
+                    return BadRequest("Invalid date format. Please provide the date in yyyy-mm-dd format.");
+                }
+
+                var rezerwacje = _context.Rezerwacje
+                    .Where(r => r.dzien == parsedDate.Date)
+                    .Select(r => new {
+                        day = r.dzien.ToString("yyyy-MM-dd"),
+                        hour = r.godz,
+                        @class = r.sala
+                    })
+                    .ToList();
+
+                return Ok(rezerwacje);
+            }
+            catch (Exception ex) {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+    }
+}
